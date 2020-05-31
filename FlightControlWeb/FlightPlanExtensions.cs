161fcebc0dfb0365c9e512969757fa8d1c0efb6d
nitsan <nitsanbh@gmail.com>
2020-05-31 17:00:54 +0300

    line interpolation works, also nice html

---
+++ FlightControlWeb/FlightPlanExtensions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace FlightControlWeb
+{
+    public static class FlightPlanExtensions
+    {
+        public static TimeSpan TotalTime(this FlightPlan plan)
+        {
+            double total_seconds = 0;
+
+            foreach (var segment in plan.segments)
+                total_seconds += segment.timespan_seconds;
+
+            return TimeSpan.FromSeconds(total_seconds);
+        }
+
+        public static (double latitude, double longitude) InterpolateLocation
+            (this FlightPlan plan, DateTime dateTime)
+        {
+            var startLoc = new Loc(
+                plan.initial_location.latitude,
+                plan.initial_location.longitude);
+
+            var timeDiff = dateTime - plan.initial_location.date_time;
+            var totalTime = timeDiff.TotalSeconds;
+
+            var loc = LinearInterpolation(startLoc, plan.segments, totalTime);
+
+            return (loc.latitude, loc.longitude);
+        }
+
+        private static Loc LinearInterpolation
+            (Loc startLoc, List<FlightSegment> segments, double totalSeconds)
+        {
+            for (int i = 0; i < segments.Count && totalSeconds >= 0; i++)
+            {
+                var segment = segments[i];
+
+                var segment_end_point = Loc.EndPointOf(segment);
+                var segment_duration = segment.timespan_seconds;
+
+                if (totalSeconds <= segment_duration)
+                {
+                    var delta = segment_end_point - startLoc;
+                    var slope = totalSeconds / segment_duration;
+
+                    return startLoc + (delta * slope);
+                }
+
+                startLoc = segment_end_point;
+                totalSeconds -= segment_duration;
+            }
+
+            throw new Exception("flight isn't in the air at the time");
+        }
+    }
+
+    internal struct Loc // yep, could have used Vector, but idk
+    {
+        public readonly double latitude, longitude;
+
+        public static Loc EndPointOf(FlightSegment segment)
+        {
+            return new Loc(segment.latitude, segment.longitude);
+        }
+
+        public Loc(double latitude, double longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public static Loc operator -(Loc locA, Loc locB)
+        {
+            return new Loc(
+                    locA.latitude - locB.latitude,
+                    locA.longitude - locB.longitude
+                );
+        }
+
+        public static Loc operator +(Loc locA, Loc locB)
+        {
+            return new Loc(
+                    locA.latitude + locB.latitude,
+                    locA.longitude + locB.longitude
+                );
+        }
+
+        public static Loc operator *(Loc loc, double scalar)
+        {
+            return new Loc(
+                    loc.latitude * scalar,
+                    loc.longitude * scalar
+                );
+        }
+    }
+}
+
+// todo somewhere: can FlightSegment's time be 0? cause that's a problem.
+//  server should reject and return some error code
---
+++ FlightControlWeb/Program.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FlightControlWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var start = new DateTime(year: 2000, month: 1, day: 1);
+
+            FlightPlan plan = new FlightPlan()
+            {
+                initial_location = new InitialPosition()
+                {
+                    date_time = start,
+                    latitude = 100,
+                    longitude = 100,
+                },
+
+                segments = new List<FlightSegment>()
+                {
+                    new FlightSegment()
+                    {
+                        latitude = 110,
+                        longitude = 150,
+                        timespan_seconds = 10
+                    },
+                    new FlightSegment()
+                    {
+                        latitude = 130,
+                        longitude = 200,
+                        timespan_seconds = 15
+                    }
+                },
+            };
+
+            var loc = plan.InterpolateLocation(start + TimeSpan.FromSeconds(15));
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("********************************");
+            Console.WriteLine(loc.latitude + ", " + loc.longitude);
+            Console.WriteLine("********************************");
+            Console.WriteLine("********************************");
+
+            //CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
