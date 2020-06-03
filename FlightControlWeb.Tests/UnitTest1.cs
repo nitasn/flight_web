97ec277acc007ca3928d94b5584d22d1f5a124ce
nitsan <nitsanbh@gmail.com>
2020-06-03 14:32:01 +0300

    commitment

--- FlightControlWeb.Tests/UnitTest1.cs
+++ FlightControlWeb.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace FlightControlWeb.Tests
 {
@@ -8,6 +10,42 @@ namespace FlightControlWeb.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            /*
+            var startTime = new DateTime(year: 2000, month: 1, day: 1);
+
+            FlightPlan plan = new FlightPlan()
+            {
+                initial_location = new InitialPosition()
+                {
+                    date_time = startTime,
+                    latitude = 100,
+                    longitude = 100,
+                },
+
+                segments = new List<FlightSegment>()
+                {
+                    new FlightSegment()
+                    {
+                        latitude = 110,
+                        longitude = 150,
+                        timespan_seconds = 10
+                    },
+                    new FlightSegment()
+                    {
+                        latitude = 130,
+                        longitude = 200,
+                        timespan_seconds = 15
+                    }
+                },
+            };
+
+            var actualResult =
+                plan.InterpolateLocation(startTime + TimeSpan.FromSeconds(15));
+
+            var expectedResult = (116.66666666666667, 166.66666666666666);
+
+            // todo compare doubles
+            */
         }
     }
 }
