7b323133c0baa85b8b07915e0826ad30a5c02255
nitsan <nitsanbh@gmail.com>
2020-06-03 14:31:06 +0300

    commitmen

--- FlightControlWeb/Program.cs
+++ FlightControlWeb/Program.cs
@@ -14,43 +14,7 @@ namespace FlightControlWeb
     {
         public static void Main(string[] args)
         {
-            var start = new DateTime(year: 2000, month: 1, day: 1);
-
-            FlightPlan plan = new FlightPlan()
-            {
-                initial_location = new InitialPosition()
-                {
-                    date_time = start,
-                    latitude = 100,
-                    longitude = 100,
-                },
-
-                segments = new List<FlightSegment>()
-                {
-                    new FlightSegment()
-                    {
-                        latitude = 110,
-                        longitude = 150,
-                        timespan_seconds = 10
-                    },
-                    new FlightSegment()
-                    {
-                        latitude = 130,
-                        longitude = 200,
-                        timespan_seconds = 15
-                    }
-                },
-            };
-
-            var loc = plan.InterpolateLocation(start + TimeSpan.FromSeconds(15));
-
-            Console.WriteLine("********************************");
-            Console.WriteLine("********************************");
-            Console.WriteLine(loc.latitude + ", " + loc.longitude);
-            Console.WriteLine("********************************");
-            Console.WriteLine("********************************");
-
-            //CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
