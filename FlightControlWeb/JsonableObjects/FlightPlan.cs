ce3a5afce6fa9c571ff78dc8e552205a614c2198
nitasn <46115487+nitasn@users.noreply.github.com>
2020-10-13 02:40:00 +0300

    Add FIXME (DateTime loses iso-string's time-zone!)

--- FlightControlWeb/JsonableObjects/FlightPlan.cs
+++ FlightControlWeb/JsonableObjects/FlightPlan.cs
@@ -41,5 +41,13 @@ namespace FlightControlWeb
 
         [JsonPropertyName("date_time")]
         public DateTime date_time { get; set; }
+        // FIXME there is a huge bug!!!
+        //  in the automatic parsing iso-string dates, the time-zone is lost!
+        //  In C#, DateTime.Parse("2020-10-12T23:30:33.594Z", ...).ToString() == "10/12/2020 11:30:33 PM"
+        //  But in JS, new Date('2020-10-12T23:30:33.594Z').toString() === "Tue Oct 13 2020 02:30:33 GMT+0300 (Israel Daylight Time)"
+        //
+        //  possible fixes: the property date_time can be a string... we can parse it ourselves. 
+        //  or, to use unix time stamps instead of iso-strings. can C#'s DateTime deal with that? probably
+        
     }
 }
